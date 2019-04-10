34f99455fe834aa9a09a04e61b1f17744f07f475
s.iglesias.perez@bbva.com <s.iglesias.perez@bbva.com>
2019-04-10 02:16:29 +0200

    LSTM bug fix

--- engines/lstm.py
+++ engines/lstm.py
@@ -213,6 +213,8 @@ def anomaly_uni_LSTM(lista_datos,num_forecast=10,desv_mse=2,train='True',name='t
                     print ('rmse', rmse)
                     print ('mae', mae)
                     if mae < best_mae:
+                            best_mae=mae
+                            print ("LSTM best new model " + str(mae))
                             best_model=model
 
 
