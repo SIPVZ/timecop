3433764292f2b73761173e882dfd2d9aa62bacfe
s.iglesias.perez@bbva.com <s.iglesias.perez@bbva.com>
2020-03-30 23:57:42 +0200

    pyramid to pmdarima

--- engines/auto_arima.py
+++ engines/auto_arima.py
@@ -1,26 +1,26 @@
 import numpy as np
 import pandas as pd
-from pyramid.arima import auto_arima
+import pmdarima as pm
 from sklearn.metrics import mean_squared_error,mean_absolute_error
 from . helpers import create_train_test
 
 def anomaly_AutoArima(lista_datos,num_fut,orig_size,desv_mse=0,train='True'):
     
     print (orig_size)
-    
+
     print ("tamanio original")
     start_point= int(orig_size) - 100
-    
+
     print ("start point " + str(start_point))
     lista_puntos = np.arange(start_point, orig_size,1)
 
     print (lista_puntos)
-    df, df_train, df_test = create_train_test(lista_puntos, lista_datos) 
-    
+    df, df_train, df_test = create_train_test(lista_puntos, lista_datos)
+
     engine_output={}
     print ("arranca")
-    
-    stepwise_model = auto_arima(df_train['valores'], start_p=1, start_q=1, max_p=3, max_q=3, m=12,
+
+    stepwise_model =  pm.auto_arima(df_train['valores'], start_p=1, start_q=1, max_p=3, max_q=3, m=12,
                               start_P=0, seasonal=True, d=1, D=1, trace=False, approx=False,
                               error_action='ignore',  # don't want to know if an order does not work
                               suppress_warnings=True,  # don't want convergence warnings
@@ -32,7 +32,7 @@ def anomaly_AutoArima(lista_datos,num_fut,orig_size,desv_mse=0,train='True'):
 
     fit_forecast_pred = stepwise_model.predict_in_sample(df_train['valores'])
     fit_forecast = pd.DataFrame(fit_forecast_pred,index = df_train.index,columns=['Prediction'])
-    
+
     future_forecast_pred = stepwise_model.predict(n_periods=len(df_test['valores']))
     future_forecast = pd.DataFrame(future_forecast_pred,index = df_test.index,columns=['Prediction'])
 
@@ -42,7 +42,7 @@ def anomaly_AutoArima(lista_datos,num_fut,orig_size,desv_mse=0,train='True'):
     mse = mean_squared_error(list_test, future_forecast_pred)
     rmse = np.sqrt(mse)
     mse_abs_test = abs(mse_test)
-    
+
     df_aler = pd.DataFrame(future_forecast_pred,index = df_test.index,columns=['expected value'])
     df_aler['step'] = df_test['puntos']
     df_aler['real_value'] = df_test['valores']
@@ -68,27 +68,27 @@ def anomaly_AutoArima(lista_datos,num_fut,orig_size,desv_mse=0,train='True'):
     max = df_aler['anomaly_score'].max()
     min = df_aler['anomaly_score'].min()
     df_aler['anomaly_score']= ( df_aler['anomaly_score'] - min ) /(max - min)
-    
+
     max = df_aler_ult['anomaly_score'].max()
     min = df_aler_ult['anomaly_score'].min()
- 
+
     df_aler_ult['anomaly_score']= ( df_aler_ult['anomaly_score'] - min ) /(max - min)
 
 
-    ############## ANOMALY FINISHED, 
+    ############## ANOMALY FINISHED,
     print ("Anomaly finished. Start forecasting")
 
     ############## FORECAST START
     updated_model = stepwise_model.fit(df['valores'])
-    
+
     filename = './models_temp/arima_' +name
     # Serialize with Pickle
     with open(filename, 'wb') as pkl:
         pickle.dump(stepwise_model, pkl)
-    
-    
+
+
     forecast = updated_model.predict(n_periods=num_fut)
-    
+
     engine_output['rmse'] = rmse
     engine_output['mse'] = mse
     engine_output['mae'] = mean_absolute_error(list_test, future_forecast_pred)
@@ -103,5 +103,5 @@ def anomaly_AutoArima(lista_datos,num_fut,orig_size,desv_mse=0,train='True'):
     testing_data  = pd.DataFrame(future_forecast_pred,index = df_test.index,columns=['expected value'])
     testing_data['step']=testing_data.index
     engine_output['debug'] = testing_data.to_dict(orient='record')
-    
+
     return (engine_output)
