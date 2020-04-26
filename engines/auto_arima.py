68359754bd06736b3be82d2fee6e0a94e3333911
s.iglesias.perez@bbva.com <s.iglesias.perez@bbva.com>
2020-04-26 23:42:43 +0200

    Autoarime bug

--- engines/auto_arima.py
+++ engines/auto_arima.py
@@ -2,6 +2,7 @@ import numpy as np
 import pandas as pd
 import pmdarima as pm
 from sklearn.metrics import mean_squared_error,mean_absolute_error
+from . engine_output_creation import engine_output_creation
 from . helpers import create_train_test
 import pickle
 
